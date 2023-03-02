07b4802d46bfb66a0264f7b6d9ce45aa9577a213
James Gillanders <109094440+jamesGub@users.noreply.github.com>
2023-03-02 13:14:49 -0500

    Add files via upload

---
+++ 23bd663f027b4b708b1bdea70966a73d_Mileage.java
@@ -0,0 +1,38 @@
+/** Name: James Gillanders
+ * Date: 02.03.2023
+ * Program Synopsis: This program is designed to calculate the gas milage of a user
+ 
+  */
+
+//Importing the Java scanner function 
+import java.util.Scanner;
+
+//Class that matches the Java file name
+public class Mileage {
+	//Our main method
+	public static void main(String[] args) {
+	//Text welcoming the user to the program and stating what it does
+	System.out.println("Welcome to the milage calculator! \n");
+	//Our scanner which we named askMiles
+	Scanner askMiles = new Scanner(System.in);
+	//First input question for the user
+	System.out.println("How many miles have you driven? : ");
+	//Double declaration for the miles, also input the miles driven
+	double miles = askMiles.nextDouble(); 
+	//Returns the miles of the user that they inputed plus some sample text
+	System.out.println("Here are your miles: " + miles);
+	//Asking the user how many gallons they have exhausted 
+	System.out.println("How many gallons have you used? : ");
+	//Double declaration for the gallons and the input for it
+	double gallons = askMiles.nextDouble();
+	//Returns the gallons plus some sample text
+	System.out.println("Here are your gallons: " + gallons);
+	//The division of the miles and gallons to return the total mileage, also a double
+	double divi = (miles / gallons); 
+	//Return of the miles per gallon, plus sample text. 
+	System.out.println("Here are your miles per gallon: " + divi);
+	
+	
+	}
+	
+}
