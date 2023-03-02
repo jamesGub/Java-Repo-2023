b45ad51ab4614edecfb9d2f01fb68dc9eead227f
James Gillanders <109094440+jamesGub@users.noreply.github.com>
2023-03-02 13:08:32 -0500

    Update and rename LabProgram.java to inductArrays.java

--- inductArrays.java
+++ inductArrays.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
-public class LabProgram {
+public class inductArrays {
    public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in);
       int[] userList = new int[20];   // List of numElement integers specified by the user
       int numElements;                // Number of integers in user's list
       // Add more variables as needed
-
-      numElements = scnr.nextInt();   // Input begins with number of integers that follow
       
-      /* Type your code here. */
+      Scanner numRead = new Scanner(System.in);
+
+
+      numElements = scnr.nextInt();   
+      
+      
       for(int i = 0; i < numElements; i++) {
     	  userList[i] = scnr.nextInt();
       }
@@ -22,3 +24,4 @@ public class LabProgram {
       
    }
 }
+//results in numbers ordered from largest to smallest so long as they are in range
